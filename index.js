aac15936c0f2e8be4e3d09a1136f0d60dee42d4b
FahimMuntashir <fahim.tech.me@gmail.com>
2020-05-24 01:37:46 +0600

    keypress

--- index.js
+++ index.js
@@ -7,55 +7,64 @@ for (var i = 0; i < numOfButtons; i++) {
   function handClick() {
     var buttonInnerHtml = this.innerHTML;
 
-    switch (buttonInnerHtml) {
-      case "w":
-
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-
-        break;
-
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play()
-        break;
-
-      case "k":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "l":
-        var kickBass = new Audio("sounds/kick-bass.mp3");
-        kickBass.play();
-        break;
-
-      default:
-        console.log(buttonInnerHtml);
-
-    }
-
+    makeSound(buttonInnerHtml);
   }
 
 }
 
 
 
-document.addEventListener("keypress", function(){
-  alert("key is pressed");
+document.addEventListener("keypress", function(event){
+  makeSound(event.key);
 });
+
+
+
+function makeSound(key){
+
+  switch (key) {
+    case "w":
+
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play()
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    default:
+      console.log(buttonInnerHtml);
+
+  }
+
+
+
+}

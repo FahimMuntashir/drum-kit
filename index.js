cd5a91e31d9b8071f1c1b43706f8dcbcc5c04b0e
FahimMuntashir <fahim.tech.me@gmail.com>
2020-05-24 16:36:20 +0600

    buttonAnimation Final

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ function makeSound(key){
 
 
 function buttonAnimation(currentKey){
- var activButton = document.querySelector("." + currentKey);
- activButton.classList.add("pressed");
+ var activeButton = document.querySelector("." + currentKey);
+ activeButton.classList.add("pressed");
+
+ setTimeout(function(){
+   activeButton.classList.remove("pressed");
+ },
+ 100
+);
 }
